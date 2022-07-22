a4acfbe68792f1067db0a5afa2e575edb4e0fd06
Nathan Clevenger <nateclev@gmail.com>
2022-07-22 15:47:57 -0500

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,15 +1,3 @@
 export default {
-  fetch: async (req) => {
-    const { pathname: domain } = new URL(req.url)
-    console.log(domain)
-    const [NS, A, AAAA, CNAME, MX] = await Promise.all([
-      fetch('https://cloudflare-dns.com/dns-query?type=NS&name=' + domain, { headers: { accept: 'application/dns-json' }}).then(res => res.json()),
-      fetch('https://cloudflare-dns.com/dns-query?type=A&name=' + domain, { headers: { accept: 'application/dns-json' }}).then(res => res.json()),
-      fetch('https://cloudflare-dns.com/dns-query?type=AAAA&name=' + domain, { headers: { accept: 'application/dns-json' }}).then(res => res.json()),
-      fetch('https://cloudflare-dns.com/dns-query?type=CNAME&name=' + domain, { headers: { accept: 'application/dns-json' }}).then(res => res.json()),
-      fetch('https://cloudflare-dns.com/dns-query?type=MX&name=' + domain, { headers: { accept: 'application/dns-json' }}).then(res => res.json()),
-    ])
-    
-    return new Response(JSON.stringify({NS, A, AAAA, CNAME, MX}, null, 2))
-  }
+  fetch: (req) => fetch('https://workers.cloudflare.com/cf.json', req.clone())
 }
